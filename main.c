5b218eb2ac557f483d9a95d4620c01c24ab8d44f
Tomas <maceiratomas@gmail.com>
2023-04-13 19:58:45 +0200

    Added player with movements

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "raylib.h"
 
+typedef struct {
+  float xPos;
+  float yPos;
+  int width;
+  int height;
+} Player;
+
 int main() {
   InitWindow(320, 320, "Kiriko che boxa il mio pene");
 
@@ -10,12 +17,32 @@ int main() {
   SetWindowSize(monitorWidth, monitorHeight);
   ToggleFullscreen();
 
+  Player player = {
+    10.0, 10.0, 10, 10,
+  };
+
   while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_A)) {
+      player.xPos -= 0.5;
+    }
+
+    if (IsKeyDown(KEY_D)) {
+      player.xPos += 0.5;
+    }
+
+    if (IsKeyDown(KEY_W)) {
+      player.yPos -= 0.5;
+    }
+
+    if (IsKeyDown(KEY_S)) {
+      player.yPos += 0.5;
+    }
+
     BeginDrawing();
 
       ClearBackground(RAYWHITE);
 
-      DrawText("SCREEN AREA", monitorWidth / 2 - MeasureText("SCREEN AREA", 32) / 2, 10, 32, RED);
+      DrawRectangle((int) player.xPos, (int) player.yPos, player.width, player.height, RED);
 
     EndDrawing();
   }
