ea74073dcb8373678766974c1821fb4460c01179
Tomas <maceiratomas@gmail.com>
2023-04-13 11:39:31 +0200

    Added fullscreen main.c

--- main.c
+++ main.c
@@ -2,14 +2,20 @@
 #include "raylib.h"
 
 int main() {
-  InitWindow(320, 320, "Prova");
+  InitWindow(320, 320, "Kiriko che boxa il mio pene");
+
+  const int monitor = GetCurrentMonitor();
+  const int monitorWidth = GetMonitorWidth(monitor), monitorHeight = GetMonitorHeight(monitor);
+
+  SetWindowSize(monitorWidth, monitorHeight);
+  ToggleFullscreen();
 
   while (!WindowShouldClose()) {
     BeginDrawing();
 
       ClearBackground(RAYWHITE);
 
-      DrawText("SCREEN AREA", 640, 10, 20, RED);
+      DrawText("SCREEN AREA", monitorWidth / 2 - MeasureText("SCREEN AREA", 32) / 2, 10, 32, RED);
 
     EndDrawing();
   }
